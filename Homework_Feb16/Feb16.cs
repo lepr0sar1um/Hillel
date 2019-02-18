9cb710fb4ada370421a92e42558feb20a4c49663
Ivan Eremenko <ivan.eremenko@together.com>
2019-02-18 13:15:11 +0200

    Added menu

--- Homework_Feb16/Feb16.cs
+++ Homework_Feb16/Feb16.cs
@@ -12,12 +12,24 @@ public static class Feb16
             const int homeWorkDate = 16;
  
             Console.WriteLine("Hi! This is my homework for {0} {1}.", homeWorkMonth, homeWorkDate);
+            
             bool repeat = true;
  
             while (repeat)
             {
                 Console.Clear();
-                Console.Write("Please, select task number between 0 and 9.\n" +
+                Console.Write("Task list:\n\n" +
+                              "0. Personal data\n" +
+                              "1. Show PI\n" +
+                              "2. Two gaps between numbers\n" +
+                              "3. Convert centimeters into meters\n" +
+                              "4. Convert days into weeks\n" +
+                              "5. Show average\n" +
+                              "6. Calculate sqrt\n" +
+                              "7. Show square count\n" +
+                              "8. Show system info\n" +
+                              "9. Show value from console\n\n" +
+                              "Please, select task number between 0 and 9.\n" +
                               "Press -1 to exit: ");
                 int selector;
  
@@ -27,7 +39,8 @@ public static class Feb16
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Please, use only numbers.\n" +
+                                      e.Message);
                     WaitForUSer();
                     continue;
                 }
@@ -179,6 +192,11 @@ public static class Feb16
             Console.WriteLine("You have entered {0}", number);
            
             WaitForUSer();
+        }
+
+        public static void TaskSelector()
+        {
+            
         }
  
         public static void WaitForUSer()
