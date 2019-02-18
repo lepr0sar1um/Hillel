f78f44db933f8db24664427487c81951a8445e4b
Ivan Eremenko <ivan.eremenko@together.com>
2019-02-18 16:26:31 +0200

    minor fixes

--- Homework_Feb16/Feb16.cs
+++ Homework_Feb16/Feb16.cs
@@ -11,24 +11,11 @@ public static class Feb16
  
             Console.WriteLine("Hi! This is my homework for {0} {1}.", homeWorkMonth, homeWorkDate);
             
-            bool repeat = true;
+            var repeat = true;
  
             while (repeat)
             {
-                Console.Clear();
-                Console.Write("Task list:\n\n" +
-                              "0. Personal data\n" +
-                              "1. Show PI\n" +
-                              "2. Two gaps between numbers\n" +
-                              "3. Convert centimeters into meters\n" +
-                              "4. Convert days into weeks\n" +
-                              "5. Show average\n" +
-                              "6. Calculate sqrt\n" +
-                              "7. Show square count\n" +
-                              "8. Show system info\n" +
-                              "9. Show value from console\n\n" +
-                              "Please, select task number between 0 and 9.\n" +
-                              "Or -1 to exit: ");
+                ShowMenu();
                 int selector;
  
                 try
@@ -114,26 +101,36 @@ public static class Feb16
  
         public static void TaskTwo()
         {
-            const int firstNumber = 7, secondNumber = 15, thirdNumber = 100;
-            Console.WriteLine("{0}  {1}  {2}", firstNumber, secondNumber, thirdNumber);
+            Console.Write("Please, input first number: ");
+            var firstNumber = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Please, input second number: ");
+            var secondNumber = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Please, input third number: ");
+            var thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("OK, let me input two gaps between these numbers: "+
+                              "{0}  {1}  {2}", firstNumber, secondNumber, thirdNumber);
             
             WaitForUSer();
         }
  
         public static void TaskThree()
         {
-            Console.Write("Please, insert count in centimeters: ");
-            string cm = Console.ReadLine();
-            int count = int.Parse(cm);
+            Console.Write("Please, insert quantity in centimeters: ");
+            var centiMeters = Convert.ToInt32(Console.ReadLine());
            
-            Console.WriteLine("Total meters in your number: " + (count / 100));
+            Console.WriteLine("Total meters in your number: " + (centiMeters / 100));
             
             WaitForUSer();
         }
  
         public static void TaskFour()
         {
-            const int days = 234, daysInAWeek = 7;
+            Console.Write("Please, input the quantity of days: ");
+            var days = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+            const int daysInAWeek = 7;
             Console.WriteLine("Full weeks = " + (days/daysInAWeek));
             
             WaitForUSer();
@@ -155,17 +152,20 @@ public static class Feb16
         public static void TaskSix()
         {
             Console.Write("Input your value: ");
-            string value = Console.ReadLine();
-            int inputedValue = Int32.Parse(value);
+            var introducedValue = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
            
-            Console.WriteLine("The square root of the number entered = " + Math.Sqrt(inputedValue));
+            Console.WriteLine("The square root of the value entered = " + Math.Sqrt(introducedValue));
             
             WaitForUSer();
         }
  
         public static void TaskSeven()
         {
-            const int sideA = 543, sideB = 130;
+            Console.Write("Please, input side A in mm: ");
+            var sideA = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+            
+            Console.Write("Please, input side B in mm: ");
+            var sideB = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
            
             Console.WriteLine("The are {0} squares", sideA / sideB);
             
@@ -183,7 +183,7 @@ public static class Feb16
         public static void TaskTwoSecondPart()
         {
             Console.Write("Enter number: ");
-            string number = Console.ReadLine();
+            var number = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
             Console.WriteLine("You have entered {0}", number);
            
             WaitForUSer();
@@ -193,6 +193,24 @@ public static class Feb16
         {
             Console.Write("Press any key to continue...");
             Console.ReadKey();
+        }
+
+        public static void ShowMenu()
+        {
+            Console.Clear();
+            Console.Write("Task list:\n\n" +
+                          "0. Personal data\n" +
+                          "1. Show PI\n" +
+                          "2. Two gaps between numbers\n" +
+                          "3. Convert centimeters into meters\n" +
+                          "4. Convert days into weeks\n" +
+                          "5. Show average\n" +
+                          "6. Calculate sqrt\n" +
+                          "7. Show square count\n" +
+                          "8. Show system info\n" +
+                          "9. Show value from console\n\n" +
+                          "Please, select task number between 0 and 9.\n" +
+                          "Or -1 to exit: ");
         }
     }
 }
