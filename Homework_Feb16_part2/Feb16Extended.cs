ccaad6b5bbc63c08568d0c1a0a5273eea447bdf4
Ivan Eremenko <ivan.eremenko@together.com>
2019-02-19 10:55:06 +0200

    another homework added

---
+++ Homework_Feb16_part2/Feb16Extended.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_Feb16_part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
