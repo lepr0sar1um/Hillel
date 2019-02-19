243dd44d710b9e2701b3362059ed8a8feae09021
Ivan Eremenko <ivan.eremenko@together.com>
2019-02-19 13:56:17 +0200

    fixes

--- Homework_Feb16_Extended/Feb16Extended.cs
+++ Homework_Feb16_Extended/Feb16Extended.cs
@@ -1,10 +1,54 @@
 ﻿using System;
 
-namespace Homework_Feb16_Extended
+namespace Homework_Feb16_part2
 {
-    static class Feb16Extended
+    public class Feb16Extended
     {
-        static void Main(string[] args)
+        private const string USER_FIRST_NAME = "Ivan";
+        private const string USER_SECOND_NAME = "Eremenko";
+
+        public static void Main(string[] args)
+        {
+            
+        }
+        
+        private static void Authorization()
+        {
+            var tryAgain = true;
+            Console.WriteLine("Please, input you name: ");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Please, input your second name: ");
+            var secondName = Console.ReadLine();
+
+            if (IsUserEquels(firstName, secondName))
+            {
+                ShowMenu();  
+            }
+            else
+            {
+                Console.WriteLine("Incorrect data. Please, try again.");
+                WaitForUSer();
+            }
+        }
+
+        private static bool IsUserEquels(string firstName, string secondName)
+        {
+            return (firstName.Equals(USER_FIRST_NAME) && secondName.Equals(USER_SECOND_NAME));
+        }
+
+        private static void WaitForUSer()
+        {
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private static void ShowMenu()
+        {
+            
+        }
+
+        private static void ExistData()
         {
             
         }
