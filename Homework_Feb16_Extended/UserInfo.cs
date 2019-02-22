56d009b87b04ab6d199a631d962256115ca145ae
Ivan Eremenko <bw0711@gmail.com>
2019-02-22 22:20:39 +0200

    fixes

--- Homework_Feb16_Extended/UserInfo.cs
+++ Homework_Feb16_Extended/UserInfo.cs
@@ -5,7 +5,7 @@ namespace Homework_Feb16_part2
 {
     public class UserInfo
     {
-        public string UserFirstName { get; } = "ivan";
-        public string UserSecondName { get; } = "eremenko";
+        public string UserFirstName = "ivan";
+        public string UserSecondName  = "eremenko";
     }
 }
