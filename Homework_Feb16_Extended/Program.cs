815f0a9b14df469b1504dafe0f47c24f2bff66ac
Ivan Eremenko <bw0711@gmail.com>
2019-02-24 16:43:03 +0200

    fixes

--- Homework_Feb16_Extended/Program.cs
+++ Homework_Feb16_Extended/Program.cs
@@ -9,7 +9,7 @@ namespace Homework_Feb16_part2
         {
             Console.WriteLine("This program can show you your accounts.\n" +
                               "You can select account, view its info, pay.\n" +
-                              "Please, login to get access");
+                              "Please, login or register");
             
             var user = new Menu();
             user.WaitForUSer();
