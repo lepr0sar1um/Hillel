82dc814daf16664852cd8154a7bbcb9cf144a3b6
Ivan Eremenko <bw0711@gmail.com>
2019-03-03 11:27:08 +0200

    taks 3 added

---
+++ Paymants/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+ 
+namespace Paymants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(UserContext db = new UserContext())
+            {
+                // создаем два объекта User
+                User user1 = new User { firstName = "Ivan", secondName = "Eremenko" };
+ 
+                // добавляем их в бд
+                db.Users.Add(user1);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+ 
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users;
+                Console.WriteLine("Список объектов:");
+                foreach(User u in users)
+                {
+                    Console.WriteLine("{0}.{1}.{2}", u.id, u.firstName, u.secondName);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
