f4b34c74706a219fe8d00b0831880ce80ae73a2d
Ivan Eremenko <bw0711@gmail.com>
2019-03-03 11:56:04 +0200

    fixes

---
+++ Feb23_Task3/TicketOptions.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Feb23_Task3
+{
+    public class TicketOptions
+    {
+        public void ParseTicketNumber()
+        {
+            while (true)
+            {
+                Console.Clear();
+                
+                Console.Write("Input ticket number: ");
+                var ticketNumber = Console.ReadLine();
+
+                try
+                {
+                    Convert.ToInt32(ticketNumber);
+                }
+                catch (Exception exception)
+                {
+                    Console.Write(exception.Message);
+                    Console.ReadLine();
+                    continue;
+                }
+                
+                if (ticketNumber.Length != 6)
+                {
+                    Console.Write("Ticket number must include 6 digits!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                var leftPart = int.Parse(ticketNumber?.Substring(
+                    0, ticketNumber.Length / 2));
+                var rightPart = int.Parse(ticketNumber?.Substring(
+                    (ticketNumber.Length) / 2, ticketNumber.Length -3));
+
+                Console.WriteLine(IsHappy(leftPart) == IsHappy(rightPart)
+                    ? "You have a happy ticket!"
+                    : "It is not a happy ticket :(");
+                
+                break;
+            }
+        }
+
+        private static int IsHappy(int value)
+        {
+            var sum = 0;
+            while (value != 0)
+            {
+                sum += value % 10;
+                value /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
