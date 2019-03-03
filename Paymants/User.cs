f94b26726a2cfac3d106045324169177177b0c07
Ivan Eremenko <bw0711@gmail.com>
2019-03-03 11:25:37 +0200

    remove trash

--- Paymants/User.cs
+++
@@ -1,9 +0,0 @@
-namespace Paymants
-{
-    public class User
-    {
-        public int id { get; set; }
-        public string firstName { get; set; }
-        public string secondName { get; set; }
-    }
-}
