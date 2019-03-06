6ec07a7dbc31ec1a00c8b1d25b8aa974e93e1727
Ivan Eremenko <bw0711@gmail.com>
2019-03-06 10:02:59 +0200

    task 8 and 9

---
+++ Feb23_Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Feb23_Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var X = false;
+            var Y = true;
+            var Z = false;
+
+            Console.WriteLine(X || Z);
+            Console.WriteLine(X && Y);
+            Console.WriteLine(X && Z);
+        }
+    }
+}
---
+++ Feb23_Task9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Feb23_Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var X = false;
+            var Y = false;
+            var Z = true;
+
+            Console.WriteLine(X || Y && !Z);
+            Console.WriteLine(!X && !Y);
+            Console.WriteLine(!(X && Z) || Y);
+            Console.WriteLine(X && !Y || Z);
+            Console.WriteLine(X && (!Y || Z));
+            Console.WriteLine(X && (!(Y || Z)));
+        }
+    }
+}
